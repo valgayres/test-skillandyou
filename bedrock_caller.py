2ef17e64cc6e1fd177d9978e6761c3eff05bd7d2
Vincent Algayres <v.algayres@gmail.com>
2024-11-29 11:13:08 +0100

    New envs

--- bedrock_caller.py
+++ bedrock_caller.py
@@ -6,8 +6,8 @@ import json
 def call(system_prompt, c):
     bedrock_runtime = boto3client(
         'bedrock-runtime',
-        config=Config(region_name="eu-west-3"), aws_access_key_id="AKIA2ZTAOFPDBJGTT7M3",
-        aws_secret_access_key="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",
+        config=Config(region_name="eu-west-3"), aws_access_key_id="AKIA2ZTAOFPDN4ADYU7X",
+        aws_secret_access_key="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",
     )
 
     model_id = 'anthropic.claude-3-sonnet-20240229-v1:0'
@@ -15,7 +15,8 @@ def call(system_prompt, c):
     um = {"role": "user", "content": c}
     messages = [um]
     response = bedrock_runtime.invoke_model(body=json.dumps(
-        {"anthropic_version": "bedrock-2023-05-31","max_tokens": max_tokens,"system": system_prompt,"messages": messages,"temperature": 0.5}
+        {"anthropic_version": "bedrock-2023-05-31", "max_tokens": max_tokens,
+            "system": system_prompt, "messages": messages, "temperature": 0.5}
     ), modelId=model_id)
     response_body = json.loads(response.get('body').read())
     return response_body['content'][0]['text']
